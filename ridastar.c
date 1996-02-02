65fc35cd1eb8df9b314f8198948b3affb6dbcab0
bart <bart>
1996-02-02 11:51:15 +0000

    add missed-case of necessary move

--- ridastar.c
+++ ridastar.c
@@ -12,7 +12,7 @@ static int depth_first(struct state *s, int c, struct protect *pp) {
     return 1;
   }
   global_protect_move(s, &p, pp);
-  /* try moving a tower top onto a correct tower */
+  /* move any tower top onto a correct tower */
   for (j = 0; j < s->n_towers; j++) {
     /* move only onto correct partial towers */
     if (score_towertop(s, j))
@@ -36,7 +36,21 @@ static int depth_first(struct state *s, int c, struct protect *pp) {
 	return 0;
       }
   }
-  /* otherwise, move something which must move to the table. */
+  /* move any must-move-twice block onto the table */
+  for (i = 0; i < s->n_towers; i++) {
+    if (score_towertop(s, i) != 2)
+      continue;
+    if (s->blocks[s->tower_tops[i]].on > -1) {
+      local_protect_move(s, &p, i, -1);
+      move(s, i, -1);
+      if (depth_first(s, c, &p))
+	return 1;
+      backout_move(s, &p);
+      /* a strictly forward move failed, so we're stuck */
+      return 0;
+    }
+  }
+  /* otherwise, try moving something which must move to the table. */
   for (i = 0; i < s->n_towers; i++)
     if (s->blocks[s->tower_tops[i]].on > -1) {
       int old_h = s->h_score;

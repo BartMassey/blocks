b7a1fe818e23e4dbee22ed4619906a6d784a86da
bart <bart>
1996-07-12 21:53:39 +0000

    user argument checking dumb_heuristic

--- blocks.c
+++ blocks.c
@@ -16,6 +16,7 @@ int verbose = 0;
 int node_limit = 0;
 int timer_expired = 0;
 int fast_heuristic = 1;
+int dumb_heuristic = 0;
 
 int main(int argc, char **argv) {
   int ida = 0;
@@ -24,50 +25,74 @@ int main(int argc, char **argv) {
 
   argv++; argc--;
   while (argc > 0)
-    if (argv[0][0] != '-')
-      abort();
-    else
+    if (argv[0][0] != '-') {
+      fprintf(stderr, "blocks: - expected\n");
+      exit(1);
+    } else
       switch (argv[0][1]) {
       case 'v':
-	if (argc < 2)
-	  abort();
+	if (argc < 2) {
+	  fprintf(stderr, "blocks: -v: argument expected\n");
+	  exit(1);
+	}
 	verbose = atoi(argv[1]);
 	argv += 2; argc -= 2;
 	break;
       case 'l':
-	if (argc < 2)
-	  abort();
+	if (argc < 2) {
+	  fprintf(stderr, "blocks: -l: argument expected\n");
+	  exit(1);
+	}
 	node_limit = atoi(argv[1]);
 	argv += 2; argc -= 2;
 	break;
       case 't':
-	if (argc < 2)
-	  abort();
+	if (argc < 2) {
+	  fprintf(stderr, "blocks: -t: argument expected\n");
+	  exit(1);
+	}
 	set_timer(atoi(argv[1]));
 	argv += 2; argc -= 2;
 	break;
       case 'a':
-	if (argc < 2)
-	  abort();
+	if (argc < 2) {
+	  fprintf(stderr, "blocks: -a: argument expected\n");
+	  exit(1);
+	}
 	if (!strcmp(argv[1], "ida"))
 	  ida = 1;
 	else if (!strcmp(argv[1], "rida"))
 	  rida = 1;
 	else if (!strcmp(argv[1], "ra"))
 	  ra = 1;
-	else
-	  abort();
+	else {
+	  fprintf(stderr, "blocks: -a: unknown algorithm %s\n", argv[1]);
+	  exit(1);
+	}
 	argv += 2; argc -= 2;
 	break;
       case 'h':
-	fast_heuristic = 0;
-	argv++; argc--;
+	if (argc < 2) {
+	  fprintf(stderr, "blocks: -h: argument expected\n");
+	  exit(1);
+	}
+	if (!strcmp(argv[1], "slow"))
+	  fast_heuristic = 0;
+	else if(!strcmp(argv[1], "dumb"))
+	  dumb_heuristic = 1;
+	else {
+	  fprintf(stderr, "blocks: -h: unknown heuristic %s\n", argv[1]);
+	  exit(1);
+	}
+	argv += 2; argc -= 2;
 	break;
       }
   read_problem();
   init_closure();
   fix_bottoms(start);
-  if (fast_heuristic)
+  if (dumb_heuristic)
+    dumb_score_state(start);
+  else if (fast_heuristic)
     score_state(start);
   else
     deluxe_score_state(start);

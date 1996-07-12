e1acaeda1adedfe4cad30c7fc74d02dff39baa21
bart <bart>
1996-07-12 17:36:42 +0000

    fixed for acc

--- extern.h
+++ extern.h
@@ -1,9 +1,10 @@
 extern void *malloc(unsigned);
 extern void free(void *);
+#ifdef __GNUC__
 extern void perror(char *);
 extern void printf(char *, ...);
 extern void fprintf(FILE *, char *, ...);
-#ifndef __GNUC__
+#else
 extern int strcmp(char *, char *);
 #endif
 extern int atoi(char *);
